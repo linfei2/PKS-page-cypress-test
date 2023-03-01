be7eeba4febb88eaa0736642f4389d7f29504fb1
Dominika Matczynska <dominika.matczynska@stxnext.pl>
2023-03-01 10:16:03 +0100

    Refactoring

--- cypress/e2e/complaint.spec.cy.js
+++ cypress/e2e/complaint.spec.cy.js
@@ -1,21 +1,20 @@
-context('Complaint form page', () => {
-    let data;
-
-    before(() => {
-        cy.fixture('complaint').then((complaintData) => {
-            data = complaintData;
-        });
-        cy.visit('formularz-reklamacji');
+describe('Complaint form page', () => {
+    beforeEach(() => {
+        cy.fixture('complaint.json').as('complaint')
+        cy.visit('formularz-reklamacji')
     })
-    it('Try to submit the form without checking required field', () => {
-        cy.get('input[name="imie_nazw"]').type(data.name);
-        cy.get('input[name="adres"]').type(data.address);
-        cy.get('input[name="email"]').type(data.email);
-        cy.get('input[name="telefon"]').type(data.phone);
-        cy.get('input[name="data_kursu"]').type(data.dateTime);
-        cy.get('input[name="trasa_kursu"]').type(data.route);
-        cy.get('textarea[name="reklamacja"]').type(data.reason);
-        cy.contains('Wyślij formularz').click()
-        cy.get('.fancyInfo').should('have.text', 'Należy zaakceptować notę prawną');
+
+    it('Try to submit the form without checking required field', function() {
+        const data = this.complaint
+
+        cy.get('input[name="imie_nazw"]').type(data.name)
+        cy.get('input[name="adres"]').type(data.address)
+        cy.get('input[name="email"]').type(data.email)
+        cy.get('input[name="telefon"]').type(data.phone)
+        cy.get('input[name="data_kursu"]').type(data.dateTime)
+        cy.get('input[name="trasa_kursu"]').type(data.route)
+        cy.get('textarea[name="reklamacja"]').type(data.reason)
+        cy.contains('button.formBlockBut', 'Wyślij formularz').click()
+        cy.get('.fancyInfo').should('have.text', 'Należy zaakceptować notę prawną')
       })
 })
--- cypress/e2e/homepage.spec.cy.js
+++ cypress/e2e/homepage.spec.cy.js
@@ -1,58 +1,54 @@
 
-context('PKS Bydgoszcz Home Page', () => {
+describe('PKS Bydgoszcz Home Page', () => {
 
   beforeEach(() => {
-    cy.visit('/');
-    cy.title().should('eq', 'PKS w Bydgoszczy Sp. z o.o.');
+    cy.visit('/')
+    cy.title().should('eq', 'PKS w Bydgoszczy Sp. z o.o.')
+
+    cy.get('form[name=rja_form_wysz]').as('searchForm')
+    cy.get('input#kodpp').as('fromField')
+    cy.get('input#kodpd').as('toField')
   })
 
   it('Switch tabs', () => {
-    cy.get('.k2_menu li').first().as('menuTab')
-    cy.get('@menuTab').should('have.class', 'aktywna')
-    cy.get('@menuTab').next().click()
+    cy.get('.k2_menu li').first().as('firstTab')
+    cy.get('@firstTab')
+      .should('have.class', 'aktywna')
+      .next().click()
+    cy.get('@firstTab').should('not.have.class', 'aktywna')
     cy.get('.k2_menu li.aktywna')
       .should('have.text', 'Rozkład jazdy')
   })
 
   it('Check if current date is displayed by default', () => {
-    const today = new Date();
+    const today = new Date()
     const currentDate = today.toISOString().split('T')[0]
 
-    cy.get('form[name=rja_form_wysz]')
-      .find('input.rjaDataPicker')
+    cy.get('input.rjaDataPicker')
       .invoke('attr', 'value')
       .should('eq', currentDate)
   })
 
   it('Valid search for connections', () => {
-    cy.get('form[name=rja_form_wysz]').as('searchForm')
-    cy.get('input#kodpp').as('fromField')
-    cy.get('input#kodpd').as('toField')
-
+    // Select origin by position on the list
     cy.get('@fromField').type('By')
     cy.get('ul#ui-id-1 > li > div')
-      .eq(0)
-      .then(($firstOption) => {
-        cy.wrap($firstOption).click()
-      })
+      .eq(0).click()
 
+    // Select destination by partial text
     cy.get('@toField').type('Ba')
-    cy.get('ul#ui-id-2 > li > div').each(($el) => {
-      if($el.text().includes('P.DW.')) {
-        cy.wrap($el).click()
-      }
-    })
-    cy.get('@searchForm').submit()
+    cy.get('ul#ui-id-2 > li > div').filter(':contains("P.DW.")').click()
 
+    cy.get('@searchForm').submit();
     cy.get('h1.tytul').should('have.text', `BYDGOSZCZ, D.A. - BARCIN, P.DW.`)
-    cy.get('table.rjaTab').should('exist')
-
+    cy.get('table.rjaTab').should('be.visible')
   })
 
   it('Invalid search for connections', () => {
-    cy.get('input#kodpp').type('Gibberish')
-    cy.get('form[name=rja_form_wysz]').submit()
+    cy.get('@fromField').type('Gibberish')
+    cy.get('@searchForm').submit()
     cy.get('.fancyInfo').should('have.text', 'Neleży poprawnie wskazać początkowy przystanek podróży')
+    cy.get('@fromField').parent().should('have.class', 'rjaWalidFalse')
   })
 
 })
